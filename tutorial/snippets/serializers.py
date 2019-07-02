0efabad529b910abdc9a2dc5122911319fb1035c
mhsze <munhou.sze@gmail.com>
2019-07-02 14:43:35 +0800

    part 5 complete

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -28,17 +28,35 @@ from django.contrib.auth.models import User
 #         instance.style = validated_data.get('style', instance.style)
 #         instance.save()
 #         return instance
-class UserSerializer(serializers.ModelSerializer):
-    # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.get())
+
+# class UserSerializer(serializers.ModelSerializer):
+#     # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.get())
+
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'snippets')
+#         # fields = "__all__"
+#         #exclude = ('username', )
+
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#     class Meta:
+#         model = Snippet
+#         fields = ('id', 'owner', 'title', 'code', 'linenos', 'language', 'style')
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
-        # fields = "__all__"
-        #exclude = ('username', )
-
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    class Meta:
-        model = Snippet
-        fields = ('id', 'owner', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'id', 'username', 'snippets')
